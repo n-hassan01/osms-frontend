ea8a6a73538be6295f55de6df78712f794870755
n-hassan01 <naimul.hassan.se@gmail.com>
2024-01-20 17:32:12 +0600

    update complete

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,14 +1,17 @@
 import { LoadingButton } from '@mui/lab';
 import { Container, IconButton, InputAdornment, Stack, TextField } from '@mui/material';
 import { MDBAccordion, MDBAccordionItem } from 'mdb-react-ui-kit';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // component
-import { comparePassword, updatePassword, updateProfileDetails, updateUsername } from '../Services/ApiServices';
+import { changePasswordService, comparePasswordService, getUserProfileDetails } from '../Services/ApiServices';
 import Iconify from '../components/iconify';
 
+import { useUser } from '../context/UserContext';
+// import { comparePasswordService } from '../Services/ApiServices';
+
 export default function SettingsPage() {
-  const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
 
   // useEffect(() => {
   //   async function fetchData() {
@@ -23,6 +26,29 @@ export default function SettingsPage() {
   //   fetchData();
   // }, []);
 
+  const [account, setAccount] = useState({});
+  const { user } = useUser();
+  console.log(user);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        if (user) {
+          const accountDetails = await getUserProfileDetails(user); // Call your async function here
+          if (accountDetails.status === 200) {
+            setAccount(accountDetails.data);
+          } // Set the account details in the component's state
+        }
+      } catch (error) {
+        // Handle any errors that might occur during the async operation
+        console.error('Error fetching account details:', error);
+      }
+    }
+
+    fetchData(); // Call the async function when the component mounts
+  }, [user]);
+  console.log(account);
+
   const navigate = useNavigate();
 
   const [showPassword, setShowPassword] = useState(false);
@@ -32,10 +58,9 @@ export default function SettingsPage() {
   const validatePhone = (phone) => phone.length >= 11;
 
   const validateOldPassword = async (passwordInfo) => {
-    const response = await comparePassword(passwordInfo);
-    console.log(response.data.value);
+    const response = await comparePasswordService(user, passwordInfo);
 
-    return response.data.value;
+    return response.status === 200 ? response.data.value : false;
   };
 
   const validatePassword = (password) => password.length >= 6;
@@ -54,10 +79,10 @@ export default function SettingsPage() {
   const [securityDetails, setSecurityDetails] = useState(initialSecurityDetails);
 
   // on value change functions
-  const onValueChangeProfileDetails = (e) => {
-    if (e.target) setUser({ ...user, [e.target.name]: e.target.value });
-    else setUser({ ...user, gender: e.value });
-  };
+  // const onValueChangeProfileDetails = (e) => {
+  //   if (e.target) setUser({ ...user, [e.target.name]: e.target.value });
+  //   else setUser({ ...user, gender: e.value });
+  // };
 
   const onValueChangeOldPassword = (e) => {
     setOldPassword(e.target.value);
@@ -67,54 +92,53 @@ export default function SettingsPage() {
     setSecurityDetails({ ...securityDetails, [e.target.name]: e.target.value });
   };
 
-  const onValueChangeAccountDetails = (e) => {
-    setUser({ ...user, name: e.target.value });
-  };
+  // const onValueChangeAccountDetails = (e) => {
+  //   setUser({ ...user, name: e.target.value });
+  // };
 
   // on submit functions
-  const submitProfileDetails = async () => {
-    // form validation
-    const { phone } = user;
-    const newErrors = {};
+  // const submitProfileDetails = async () => {
+  //   // form validation
+  //   const { phone } = user;
+  //   const newErrors = {};
 
-    // Validate phone
-    if (phone && !validatePhone(phone)) {
-      newErrors.phone = 'Invalid phone number';
-    }
+  //   // Validate phone
+  //   if (phone && !validatePhone(phone)) {
+  //     newErrors.phone = 'Invalid phone number';
+  //   }
 
-    // Check if there are any errors
-    if (Object.keys(newErrors).length === 0) {
-      console.log(user);
-      try {
-        const profileDetails = {
-          profession: user.profession ? user.profession : '',
-          address: user.address ? user.address : '',
-          phone: user.phone ? user.phone : '',
-          age: user.age ? user.age : '',
-          gender: user.gender ? user.gender : '',
-        };
-        const response = await updateProfileDetails(user.email, profileDetails);
+  //   // Check if there are any errors
+  //   if (Object.keys(newErrors).length === 0) {
+  //     console.log(user);
+  //     try {
+  //       const profileDetails = {
+  //         profession: user.profession ? user.profession : '',
+  //         address: user.address ? user.address : '',
+  //         phone: user.phone ? user.phone : '',
+  //         age: user.age ? user.age : '',
+  //         gender: user.gender ? user.gender : '',
+  //       };
+  //       const response = await updateProfileDetails(user.email, profileDetails);
 
-        const alertMessage =
-          response.status === 200 ? 'Profile updated successfully!' : 'Service failed! Try again later';
+  //       const alertMessage =
+  //         response.status === 200 ? 'Profile updated successfully!' : 'Service failed! Try again later';
 
-        alert(alertMessage);
-      } catch (err) {
-        alert('Service failed! Try again later');
-      }
+  //       alert(alertMessage);
+  //     } catch (err) {
+  //       alert('Service failed! Try again later');
+  //     }
 
-      navigate('/dashboard/profile', { replace: true });
-    } else {
-      setErrors(newErrors);
-    }
-  };
+  //     navigate('/dashboard/profile', { replace: true });
+  //   } else {
+  //     setErrors(newErrors);
+  //   }
+  // };
 
   const updateSecurityDetails = async () => {
     const { password, confirmPassword } = securityDetails;
     const newErrors = {};
 
     const oldPasswordInfo = {
-      email: user.email,
       password: oldPassword,
     };
     const isValidOldPassword = await validateOldPassword(oldPasswordInfo);
@@ -137,11 +161,11 @@ export default function SettingsPage() {
     if (Object.keys(newErrors).length === 0) {
       try {
         const newPasswordInfo = {
-          email: user.email,
+          // email: user.email,
           newPassword: password,
         };
 
-        const response = await updatePassword(newPasswordInfo);
+        const response = await changePasswordService(user, newPasswordInfo);
 
         const alertMessage = response.status === 200 ? response.data.message : 'Service failed! Try again later';
 
@@ -152,33 +176,33 @@ export default function SettingsPage() {
         alert('Service failed! Try again later');
       }
 
-      navigate('/dashboard/profile', { replace: true });
+      navigate('/dashboard/', { replace: true });
     } else {
       setErrors(newErrors);
     }
   };
 
-  const updateAccountDetails = async () => {
-    try {
-      const usernameInfo = {
-        email: user.email,
-        name: user.name,
-      };
+  // const updateAccountDetails = async () => {
+  //   try {
+  //     const usernameInfo = {
+  //       email: user.email,
+  //       name: user.name,
+  //     };
 
-      const response = await updateUsername(usernameInfo);
+  //     const response = await updateUsername(usernameInfo);
 
-      const alertMessage = response.status === 200 ? response.data.message : 'Service failed! Try again later';
+  //     const alertMessage = response.status === 200 ? response.data.message : 'Service failed! Try again later';
 
-      alert(alertMessage);
-    } catch (err) {
-      console.log(err.message);
+  //     alert(alertMessage);
+  //   } catch (err) {
+  //     console.log(err.message);
 
-      alert('Service failed! Try again later');
-    }
+  //     alert('Service failed! Try again later');
+  //   }
 
-    navigate('/dashboard/profile', { replace: true });
-    window.location.reload();
-  };
+  //   navigate('/dashboard/profile', { replace: true });
+  //   window.location.reload();
+  // };
 
   return (
     <Container>
