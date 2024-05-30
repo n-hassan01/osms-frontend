dca4df08d57b90ae82d4f9d272dc2a2f351f7bb8
n-hassan01 <naimul.hassan.se@gmail.com>
2024-05-30 15:49:00 +0600

    dev

--- src/sections/@dashboard/items/AddSystemItemsDialog.js
+++ src/sections/@dashboard/items/AddSystemItemsDialog.js
@@ -160,9 +160,7 @@ export default function ResponsiveDialog() {
       };
       const response = await addSystemItemsDetails(requestBody);
 
-      if (response.status === 200) {
-        console.log(response.data);
-
+      if (response.status === 200 && filteredArray.length > 0) {
         const remainingItems = [];
 
         for (const lineInfo of filteredArray) {
