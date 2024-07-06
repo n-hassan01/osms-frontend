5f83d702f4ef7a61dfbe1f70366f2f2386f2d2c5
Ahmed Raihan <ahmedraihanalif@gmail.com>
2024-07-06 15:59:52 +0600

    Add Excel Data

--- src/pages/ShowExcelFile.js
+++ src/pages/ShowExcelFile.js
@@ -1,54 +1,28 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable camelcase */
 
 import { filter } from 'lodash';
 import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useNavigate } from 'react-router-dom';
-// import { read, utils } from 'xlsx';
+import { read, utils } from 'xlsx';
 // @mui
 
-import {
-    Card,
-    Checkbox,
-    Container,
-    IconButton,
-    MenuItem,
-    Paper,
-    Popover,
-    Stack,
-    Table,
-    TableBody,
-    TableCell,
-    TableContainer,
-    TablePagination,
-    TableRow,
-    Typography
-} from '@mui/material';
+import { Card, Container, Stack, Table, TableBody, TableContainer, TablePagination, Typography } from '@mui/material';
 
 // components
-
+import ExcelListHead from 'src/sections/@dashboard/user/ExcelListHead';
+import ExcelListToolbar from 'src/sections/@dashboard/user/ExcelListToolbar';
 import { getHrLocationsDetailsService } from '../Services/Admin/GetAllHrLocations';
-import Iconify from '../components/iconify';
 import Scrollbar from '../components/scrollbar';
-import { UserListHead, UserListToolbar } from '../sections/@dashboard/user';
+
 // ----------------------------------------------------------------------
 
 const TABLE_HEAD = [
-  { id: 'location_code', label: 'Location Code', alignRight: false },
-
-  { id: 'description', label: 'Description', alignRight: false },
-
-  { id: 'inactive_date', label: 'Inactive Date', alignRight: false },
-  { id: 'address_line_1', label: 'Address Line1', alignRight: false },
-  { id: 'address_line_2', label: 'Address Line2', alignRight: false },
-  { id: 'address_line_3', label: 'Address Line3', alignRight: false },
-  { id: 'town_or_city', label: 'Town Or City', alignRight: false },
-  { id: 'country', label: 'Country', alignRight: false },
-  { id: 'postal_code', label: 'Postal Code', alignRight: false },
-  { id: 'telephone_number_1', label: 'Telephone Number1', alignRight: false },
-  { id: 'action', label: 'Action', alignRight: false },
-
-  { id: '' },
+  { id: 'id', label: 'ID', alignRight: false },
+  { id: 'name', label: 'NAME', alignRight: false },
+  { id: 'age', label: 'AGE', alignRight: false },
+  { id: 'value', label: 'VALUE', alignRight: false },
 ];
 const selectedUsers = [];
 
@@ -108,23 +82,23 @@ export default function ShowExcelFile() {
   const [isDisableBan, setIsDisableBan] = useState(false);
 
   const [selectedUserEmail, setSelectedUserEmail] = useState('');
-  // const file_type = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel'];
-  // const handleChange = (e) => {
-  //   const selected_file = e.target.files[0];
-  //   console.log(selected_file.type);
-  //   if (selected_file && file_type.includes(selected_file.type)) {
-  //     const reader = new FileReader();
-  //     reader.onload = (e) => {
-  //       const workbook = read(e.target.result);
-  //       const sheet = workbook.SheetNames;
-  //       if (sheet.length) {
-  //         const data = utils.sheet_to_json(workbook.Sheets[sheet[0]]);
-  //         setExceldata(data);
-  //       }
-  //     };
-  //     reader.readAsArrayBuffer(selected_file);
-  //   }
-  // };
+  const file_type = ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.ms-excel'];
+  const handleChange = (e) => {
+    const selected_file = e.target.files[0];
+    console.log(selected_file.type);
+    if (selected_file && file_type.includes(selected_file.type)) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const workbook = read(e.target.result);
+        const sheet = workbook.SheetNames;
+        if (sheet.length) {
+          const data = utils.sheet_to_json(workbook.Sheets[sheet[0]]);
+          setExceldata(data);
+        }
+      };
+      reader.readAsArrayBuffer(selected_file);
+    }
+  };
 
   console.log(exceldata);
   useEffect(() => {
@@ -229,51 +203,15 @@ export default function ShowExcelFile() {
       <Container>
         <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
           <Typography variant="h4" gutterBottom>
-            Locations
+            Excel Table
           </Typography>
           <div>
-            {/* <input type="file" onChange={handleChange} /> */}
-
-            {/* <Button
-              style={{ backgroundColor: 'lightgray', color: 'black', padding: '9px' }}
-              color="primary"
-              startIcon={<Iconify icon="eva:plus-fill" />}
-              variant="text"
-              onClick={() => {
-                navigate(`/dashboard/addhrlocations/null`);
-              }}
-            >
-              Add Location
-            </Button> */}
-          </div>
-          <div>
-            <table>
-              <thead>
-                <th>ID</th>
-                <th>AGE</th>
-                <th>NAME</th>
-                <th>VALUE</th>
-              </thead>
-              <tbody>
-                {exceldata.length ? (
-                  exceldata.map((info,index) => (
-                    <tr key={index}>
-                      <td>{info.ID}</td>
-                      <td>{info.AGE}</td>
-                      <td>{info.NAME}</td>
-                      <td>{info.VALUE}</td>
-                    </tr>
-                  ))
-                ) : (
-                  <h2>Data Not Present</h2>
-                )}
-              </tbody>
-            </table>
+            <input type="file" onChange={handleChange} />
           </div>
         </Stack>
 
         <Card>
-          <UserListToolbar
+          <ExcelListToolbar
             numSelected={selected.length}
             filterName={filterName}
             onFilterName={handleFilterByName}
@@ -283,7 +221,7 @@ export default function ShowExcelFile() {
           <Scrollbar>
             <TableContainer sx={{ minWidth: 800 }}>
               <Table>
-                <UserListHead
+                <ExcelListHead
                   order={order}
                   orderBy={orderBy}
                   headLabel={TABLE_HEAD}
@@ -292,112 +230,27 @@ export default function ShowExcelFile() {
                   onRequestSort={handleRequestSort}
                   onSelectAllClick={handleSelectAllClick}
                 />
+
                 <TableBody>
-                  {filteredUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                    const {
-                      location_id,
-                      location_code,
-
-                      description,
-
-                      inactive_date,
-                      address_line_1,
-                      address_line_2,
-                      address_line_3,
-                      town_or_city,
-                      country,
-                      postal_code,
-                      telephone_number_1,
-                    } = row;
-                    const rowValues = [
-                      location_id,
-                      location_code,
-
-                      description,
-
-                      inactive_date,
-                      address_line_1,
-                      address_line_2,
-                      address_line_3,
-                      town_or_city,
-                      country,
-                      postal_code,
-                      telephone_number_1,
-                    ];
-
-                    const selectedUser = selected.indexOf(location_id) !== -1;
-
-                    return (
-                      <TableRow hover key={location_id} tabIndex={-1} role="checkbox">
-                        <TableCell padding="checkbox">
-                          <Checkbox checked={selectedUser} onChange={(event) => handleClick(event, location_id)} />
-                        </TableCell>
-
-                        <TableCell align="left">{location_code}</TableCell>
-
-                        <TableCell align="left">{description}</TableCell>
-
-                        <TableCell align="left">{inactive_date}</TableCell>
-                        <TableCell align="left">{address_line_1}</TableCell>
-                        <TableCell align="left">{address_line_2}</TableCell>
-                        <TableCell align="left">{address_line_3}</TableCell>
-                        <TableCell align="left">{town_or_city}</TableCell>
-                        <TableCell align="left">{country}</TableCell>
-                        <TableCell align="left">{postal_code}</TableCell>
-                        <TableCell align="left">{telephone_number_1}</TableCell>
-
-                        <TableCell align="right">
-                          <IconButton
-                            size="large"
-                            color="primary"
-                            onClick={() => {
-                              const locationId = location_id;
-                              navigate(`/dashboard/addhrlocations/${locationId}`);
-                            }}
-                          >
-                            <Iconify icon={'tabler:edit'} />
-                          </IconButton>
-                        </TableCell>
-
-                        <Popover
-                          open={Boolean(open)}
-                          anchorEl={open}
-                          onClose={handleCloseMenu}
-                          anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-                          transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-                          PaperProps={{
-                            sx: {
-                              p: 1,
-                              width: 140,
-                              '& .MuiMenuItem-root': {
-                                px: 1,
-                                typography: 'body2',
-                                borderRadius: 0.75,
-                              },
-                            },
-                          }}
-                        >
-                          <MenuItem sx={{ color: 'success.main' }} disabled={isDisableApprove} onClick={approveUser}>
-                            <Iconify icon={'mdi:approve'} sx={{ mr: 2 }} />
-                            Appoved
-                          </MenuItem>
-
-                          <MenuItem sx={{ color: 'error.main' }} disabled={isDisableBan} onClick={banUser}>
-                            <Iconify icon={'mdi:ban'} sx={{ mr: 2 }} />
-                            Banned
-                          </MenuItem>
-                        </Popover>
-                      </TableRow>
-                    );
-                  })}
-                  {emptyRows > 0 && (
-                    <TableRow style={{ height: 53 * emptyRows }}>
-                      <TableCell colSpan={6} />
-                    </TableRow>
+                  {exceldata.length ? (
+                    exceldata.map((info, index) => (
+                      <tr key={index}>
+                        <td>{info.ID}</td>
+                        <td>{info.AGE}</td>
+                        <td>{info.NAME}</td>
+                        <td>{info.VALUE}</td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="4" style={{ textAlign: 'center', height: '100px', verticalAlign: 'middle' }}>
+                        <h2>Data Not Present</h2>
+                      </td>
+                    </tr>
                   )}
                 </TableBody>
 
-                {isNotFound && (
+                {/* {isNotFound && (
                   <TableBody>
                     <TableRow>
                       <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
@@ -419,7 +272,7 @@ export default function ShowExcelFile() {
                       </TableCell>
                     </TableRow>
                   </TableBody>
-                )}
+                )} */}
               </Table>
             </TableContainer>
           </Scrollbar>
