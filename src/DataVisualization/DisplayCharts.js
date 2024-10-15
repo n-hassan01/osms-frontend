c4c8168df8bd1f3bd478fbffc57e3b6c87e9bb47
Ahmed Raihan Alif <aalif191133@bscse.uiu.ac.bd>
2024-10-15 20:46:23 +0600

    add changes

--- src/DataVisualization/DisplayCharts.js
+++ src/DataVisualization/DisplayCharts.js
@@ -23,6 +23,8 @@ import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 import MuiAccordion from '@mui/material/Accordion';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import { FieldChooser, PivotGrid } from 'devextreme-react/pivot-grid';
+import 'devextreme/dist/css/dx.light.css';
 import { styled } from '@mui/material/styles';
 import { format, parse } from 'date-fns';
 import * as React from 'react';
@@ -57,6 +59,7 @@ import {
   getRegionService,
   getShopsListService,
   getUserProfileDetails,
+  getDrillDownCustomerDepositAnalytic,
 } from '../Services/ApiServices';
 // components
 import Progressbar from '../components/ProgressBar/Progress_bar';
@@ -268,6 +271,191 @@ export default function DisplayCharts() {
     fetchData(); // Call the async function when the component mounts
   }, [user]);
   console.log(summaryCustomerList);
+  const [drillDownData, setDrillDownData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        if (account) {
+          const response = await getDrillDownCustomerDepositAnalytic(user);
+          if (response.status === 200) {
+            setDrillDownData(response.data);
+          }
+          console.log(response.data);
+        }
+      } catch (error) {
+        console.error('Error fetching account details:', error);
+      }
+    }
+
+    fetchData();
+  }, [account]);
+
+  const handleContentReady = (e) => {
+    const rowHeaders = document.querySelectorAll('.dx-row-total.dx-grandtotal');
+    // const rowForTotal = document.getElementsByClassName('dx-row-total dx-grandtotal dx-last-cell');
+    // rowForTotal.style.backgroundColor = 'red';
+    rowHeaders.forEach((header) => {
+      header.style.backgroundColor = 'darkblue';
+      header.style.color = 'white'; // Optional: Change text color to white for better visibility
+    });
+
+    // Targeting column headers and applying a red background
+    const columnHeaders = document.querySelectorAll('#pivotGrid .dx-pivotgrid-area .dx-area-column .dx-area-field');
+    columnHeaders.forEach((header) => {
+      header.style.backgroundColor = 'red';
+      header.style.color = 'white'; // Optional: Change text color to white for better visibility
+    });
+    // Hide the grand total row
+    console.log(e.component.getDataSource());
+    console.log(document.getElementsByClassName('dx-row-total dx-grandtotal'));
+    // const paragraph = document.getElementsByClassName('dx-row-total dx-grandtotal');
+    // paragraph.disabled = false;
+    // paragraph.classList.toggle('custom-background');
+    // e.component.getDataSource().collapseAll('column');
+    //   const grandTotalRow = document.querySelector('dx-row-total dx-grandtotal');
+    // if (grandTotalRow) {
+    //   grandTotalRow.style.display = 'none';
+    // }
+
+    const elements = document.querySelectorAll('.dx-row-total.dx-grandtotal');
+    // const grandTotalRow = document.querySelector('.dx-column-grand-total.dx-row-total');
+    // if (grandTotalRow) {
+    //   grandTotalRow.style.visibility = 'hidden';
+    // }
+    elements.forEach((element) => {
+      const elements = document.querySelector(
+        '#pivotGrid > div.dx-pivotgrid-container > table > tr:nth-child(3) > td.dx-area-column-cell > div > div > div > div.dx-scrollable-content > table > thead > tr:nth-child(1) > td'
+      );
+      //   elements.style.visibility = 'hidden';
+      elements.innerText = 'All Deposites According to the Time Period';
+      element.style.fontWeight = 'bold';
+      // elements.style.fontWeight = 'bold';
+      console.log(elements);
+      // if (element.innerText.trim() === 'Grand Total') {
+      //   const grandTotalRow = element.closest('.dx-row-total');
+      //   grandTotalRow.style.visibility = 'hidden';
+      // }
+    });
+    //  const elements2=document.querySelector('#pivotGrid > div.dx-pivotgrid-container > table > tr.dx-bottom-row > td.dx-area-row-cell.dx-bottom-border > div > div > div > div.dx-scrollable-content > table > tbody > tr:nth-child(3) > td');
+    //  elements2.innerText = 'Total';
+
+    // elements.forEach((element) => {
+    //   if (element.innerText.trim() === 'Grand Total') {
+    //     element.innerText = 'Deposite According to the Time Period'; // Change the inner text to the new name
+    //     element.style.fontWeight = 'bold';
+    //   }
+    // });
+    // dx-row-total dx-grandtotal dx-last-cell
+    // dx-row-total dx-grandtotal
+    // elements.forEach((element) => {
+    //   if (element.innerText.trim() === 'Grand Total') {
+    //     // Check if it's a row or column grand total
+    //     const parentCell = element.closest('.dx-row-total');
+    //     if (parentCell.classList.contains('dx-column-grand-total')) {
+    //       element.innerText = 'Deposite According to the Time Period'; // Change the inner text to the new name
+
+    //     }
+    //   }
+    // });
+
+    // const elements = document.querySelectorAll('.dx-row-total.dx-grandtotal');
+    // elements.forEach((element) => {
+    //   if (element.innerText.trim() === 'Grand Total') {
+    //     const grandTotalRow = element.closest('.dx-row-total');
+    //     grandTotalRow.style.visibility = 'hidden';
+    //   }
+    // });
+  };
+
+  // Modify data source structure
+  const samplesData = drillDownData.map((item) => ({
+    __rowHeader__: 'Customer Group',
+    __rowsHeader__: 'Customer Name',
+    __rowssHeader__: 'Deposite Type Name',
+    __colHeader__: 'Today Deposite',
+    __col2Header__: 'Seven Day Deposite',
+    __col3Header__: 'Monthly Deposite',
+    customerGroup: item.customer_group,
+    customerName: item.customer_name,
+    depositDate: item.deposit_date,
+    depositTypeName: item.deposit_type_name,
+    todaysDeposit: item.todays_deposit,
+    sevenDayDeposit: item.seven_day_deposit,
+    monthlyDeposit: item.monthly_deposit,
+  }));
+
+  const datasSource = {
+    store: {
+      type: 'array',
+      key: 'depositDate',
+      data: samplesData,
+    },
+    fields: [
+      {
+        caption: 'Row Header',
+        dataField: '__rowHeader__',
+        area: 'row',
+      },
+      {
+        caption: 'Customer Group ',
+        dataField: 'customerGroup',
+        area: 'row',
+      },
+
+      {
+        caption: 'Row Header',
+        dataField: '__rowsHeader__',
+        area: 'row',
+      },
+
+      {
+        caption: 'Customer Name',
+        dataField: 'customerName',
+        area: 'row',
+      },
+      {
+        caption: 'Row Header',
+        dataField: '__rowssHeader__',
+        area: 'row',
+      },
+      {
+        caption: 'Deposit Type Name',
+        dataField: 'depositTypeName',
+        area: 'row',
+      },
+
+      {
+        caption: "Today's Deposit",
+        dataField: 'todaysDeposit',
+        area: 'data',
+        dataType: 'number',
+        summaryType: 'sum',
+        format: 'currency',
+      },
+
+      // Seven Day Deposit
+      {
+        caption: 'Seven Day Deposit',
+        dataField: 'sevenDayDeposit',
+        area: 'data',
+        dataType: 'number',
+        summaryType: 'sum',
+        format: 'currency',
+      },
+
+      // Monthly Deposit
+      {
+        caption: 'Monthly Deposit',
+        dataField: 'monthlyDeposit',
+        area: 'data',
+        dataType: 'number',
+        summaryType: 'sum',
+        format: 'currency',
+      },
+    ],
+
+    // showColumnGrandTotals: false,
+  };
 
   const [total, setTotal] = useState(null); // Initial state is null, not an array
   const [loadingScreen, setLoadingScreen] = useState(true); // Loading state to track async operation
@@ -1394,79 +1582,31 @@ export default function DisplayCharts() {
             </Accordion>
             <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
               <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-                <Typography>Collapsible Group Item #3</Typography>
+                <Typography>DrillDownData</Typography>
               </AccordionSummary>
               <AccordionDetails>
-                <Scrollbar>
-                  <TableContainer sx={{ minWidth: 800 }}>
-                    <Table ref={tableref}>
-                      <UserListHead
-                        order={order}
-                        orderBy={orderBy}
-                        headLabel={TABLE_HEAD_SUMMARY}
-                        rowCount={filteredSummaryUsers.length}
-                        numSelected={selected.length}
-                        onRequestSort={handleRequestSort}
-                        onSelectAllClick={handleSelectAllClick}
-                        enableReadonly
+                <>
+                  <PivotGrid
+                    id="pivotGrid"
+                    showBorders
+                    dataSource={datasSource}
+                    allowSorting
+                    allowSortingBySummary
+                    allowFiltering
+                    height={600}
+                    width={'100%'}
+                    onContentReady={handleContentReady}
+                  >
+                    {datasSource.fields.map((field) => (
+                      <FieldChooser
+                        key={field.dataField}
+                        dataField={field.dataField}
+                        caption={field.caption}
+                        area={field.area}
                       />
-                      <TableBody>
-                        {filteredSummaryUsers.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-                          const { deposit_amount, target_amount, customer_group } = row;
-
-                          const selectedUser = selected.indexOf(customer_group) !== -1;
-
-                          return (
-                            <TableRow hover key={customer_group} tabIndex={-1} role="checkbox" selected={selectedUser}>
-                              <TableCell align="left" style={{ whiteSpace: 'nowrap' }}>
-                                {customer_group}
-                              </TableCell>
-                              <TableCell align="left" style={{ whiteSpace: 'nowrap' }}>
-                                {getFormattedPrice(deposit_amount)}
-                              </TableCell>
-                              <TableCell align="left" style={{ whiteSpace: 'nowrap' }}>
-                                {getFormattedPrice(target_amount)}
-                              </TableCell>
-                            </TableRow>
-                          );
-                        })}
-                        {emptyRows > 0 && (
-                          <TableRow style={{ height: 53 * emptyRows }}>
-                            <TableCell colSpan={6} />
-                          </TableRow>
-                        )}
-                      </TableBody>
-
-                      {isNotFound && (
-                        <TableBody>
-                          <TableRow>
-                            <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                              <Paper sx={{ textAlign: 'center' }}>
-                                <Typography variant="h6" paragraph>
-                                  Not found
-                                </Typography>
-                                <Typography variant="body2">
-                                  No results found for &nbsp;
-                                  <strong>&quot;{filterName}&quot;</strong>.
-                                  <br /> Try checking for typos or using complete words.
-                                </Typography>
-                              </Paper>
-                            </TableCell>
-                          </TableRow>
-                        </TableBody>
-                      )}
-                    </Table>
-                  </TableContainer>
-                  <TablePagination
-                    rowsPerPageOptions={[5, 10, 25]}
-                    component="div"
-                    count={filteredSummaryUsers.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                  />
-                </Scrollbar>
+                    ))}
+                  </PivotGrid>
+                </>
               </AccordionDetails>
             </Accordion>
           </div>
