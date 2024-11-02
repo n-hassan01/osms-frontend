b725c96d546f4ba03034e97bd73ef5bc3a69c803
n-hassan01 <naimul.hassan.se@gmail.com>
2024-11-02 12:42:34 +0600

    dev

--- src/sections/@dashboard/user/AddFndUser.js
+++ src/sections/@dashboard/user/AddFndUser.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-plusplus */
 /* eslint-disable no-undef */
 
-import { Container, Grid, Stack, Typography } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 import Button from '@mui/material/Button';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
@@ -17,6 +17,8 @@ import {
   getUserProfileDetails,
 } from '../../../Services/ApiServices';
 import { useUser } from '../../../context/UserContext';
+// styles
+import '../../../_css/Utils.css';
 
 export default function AddFndUser() {
   const { user } = useUser();
@@ -234,21 +236,20 @@ export default function AddFndUser() {
     alert('Successfully Added!!!');
     navigate('/dashboard/showfnduser');
 
-    window.location.reload();
-
     setOpen(false);
   };
 
   return (
     <div>
-      <Container>
-        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={3}>
-          <Typography variant="h4" gutterBottom>
+      <Container className="marginZero indexing fullWidth">
+        {/* <Stack direction="row" alignItems="center" justifyContent="space-between" mb={3}> */}
+        {/* <Typography variant="h4" gutterBottom>
             User Add
-          </Typography>
-        </Stack>
-        <Grid item xs={3}>
+          </Typography> */}
+        {/* </Stack> */}
+        <Grid item xs={2}>
           <Button
+            className="whiteSpace-nowrap"
             style={{ marginRight: '10px', fontWeight: 'bold', color: 'black', backgroundColor: 'lightgray' }}
             onClick={() => {
               handleAddRow();
@@ -258,6 +259,7 @@ export default function AddFndUser() {
           </Button>
 
           <Button
+            className="whiteSpace-nowrap"
             style={{ marginRight: '10px', fontWeight: 'bold', color: 'black', backgroundColor: 'lightgray' }}
             onClick={handleClose}
           >
@@ -266,7 +268,7 @@ export default function AddFndUser() {
         </Grid>
 
         <div>
-          <form className="form-horizontal" style={{ marginTop: '5%' }}>
+          <form className="form-horizontal" style={{ marginTop: '10px' }}>
             <div className="table-responsive">
               <table className="table table-bordered table-striped table-highlight">
                 <thead>
